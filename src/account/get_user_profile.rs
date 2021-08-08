547d2c096cf08635bdb3b7e2d7b4637c6419f72b
maccam912 <maccam912@gmail.com>
2021-08-07 21:50:18 -0500

    initial commit

---
+++ src/account/get_user_profile.rs
@@ -0,0 +1,82 @@
+#![allow(non_camel_case_types)]
+
+use chrono::{DateTime, Utc};
+use eyre::Result;
+use serde::{Deserialize, Serialize};
+
+use crate::build_request;
+
+#[derive(Debug, Serialize, Deserialize)]
+enum Classification {
+    individual,
+    entity,
+    joint_survivor,
+    traditional_ira,
+    roth_ira,
+    rollover_ira,
+    sep_ira,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum Status {
+    active,
+    closed,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum Type {
+    cash,
+    margin,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Account {
+    account_number: String,
+    classification: Classification,
+    date_created: DateTime<Utc>,
+    day_trader: bool,
+    option_level: u8,
+    status: Status,
+    #[serde(alias = "type")]
+    account_type: Type,
+    last_update_date: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+enum AccountEnum {
+    Vec(Account),
+    Account,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Profile {
+    id: String,
+    name: String,
+    account: AccountEnum,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserProfile {
+    profile: Profile,
+}
+
+pub async fn get_user_profile() -> Result<UserProfile> {
+    println!(
+        "{:?}",
+        build_request("user/profile").send().await?.text().await?
+    );
+    let response: UserProfile = build_request("user/profile").send().await?.json().await?;
+
+    Ok(response)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::account::get_user_profile::get_user_profile;
+
+    #[tokio::test]
+    async fn test_get_user_profile() {
+        let response = get_user_profile().await.unwrap();
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use once_cell::sync::Lazy;
+use reqwest::RequestBuilder;
+
+const VERSION: &str = "v1";
+
+static CLIENT: Lazy<reqwest::Client> = Lazy::new(reqwest::Client::new);
+
+static CONFIG: Lazy<config::Config> = Lazy::new(|| {
+    config::Config::default()
+        .with_merged(config::File::with_name("Config"))
+        .unwrap_or_default()
+});
+
+static TOKEN: Lazy<String> = Lazy::new(|| CONFIG.get_str("token").unwrap());
+
+static ENDPOINT: Lazy<String> = Lazy::new(|| CONFIG.get_str("endpoint").unwrap());
+
+fn endpoint(path: &str) -> String {
+    let endpoint: &str = &ENDPOINT;
+    format!("{}/{}/{}", endpoint, VERSION, path)
+}
+
+fn build_request(path: &str) -> RequestBuilder {
+    let token: &str = &TOKEN;
+    let mut request = CLIENT.get(endpoint(path));
+    request = request.header("Accept", "application/json");
+    request = request.header("Authorization", format!("Bearer {}", token));
+    request
+}
+
+pub mod account;
