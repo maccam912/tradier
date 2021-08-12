95ba772a5d4faa10f8e745c5ac30e5eb83fe66b4
maccam912 <maccam912@gmail.com>
2021-08-11 21:12:16 -0500

    trying to drop one part of path

--- src/account/mod.rs
+++ src/account/mod.rs
@@ -1,4 +1,11 @@
-pub mod get_balances;
-pub mod get_history;
-pub mod get_positions;
-pub mod get_user_profile;
+mod get_balances;
+pub use get_balances::get_balances;
+
+mod get_history;
+pub use get_history::get_history;
+
+mod get_positions;
+pub use get_positions::get_positions;
+
+mod get_user_profile;
+pub use get_user_profile::get_user_profile;
